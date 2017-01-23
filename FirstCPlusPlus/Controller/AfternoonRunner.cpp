d3361f78ec0035e0f7d74676b0a82c318405b7f2
Asmi0816 <kidwriter2020@gmail.com>
2017-01-23 12:50:03 -0700

    Got rid of useless imports and made a controller

--- FirstCPlusPlus/Controller/AfternoonRunner.cpp
+++ FirstCPlusPlus/Controller/AfternoonRunner.cpp
@@ -1,16 +1,15 @@
 //
-//  main.cpp
+//  AfternoonRunner.cpp
 //  FirstCPlusPlus
 //
 //  Created by Smith, Arick on 1/23/17.
 //  Copyright © 2017 CTEC. All rights reserved.
 //
 
-#include <iostream>
-
 int main()
 {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+  
+    
+    
     return 0;
 }
---
+++ FirstCPlusPlus/Controller/FirstController.cpp
@@ -0,0 +1,8 @@
+//
+//  FirstController.cpp
+//  FirstCPlusPlus
+//
+//  Created by Smith, Arick on 1/23/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
